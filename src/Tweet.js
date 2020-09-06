df277e639b36d8e8509afdda9bc0a0f0c6ac503d
ishan-jaiswal <devhjaiswal@gmail.com>
2020-09-07 00:12:01 +0530

    added datetimepicker

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,7 +1,8 @@
 import React from 'react';
 //import { makeStyles } from '@material-ui/core/styles';
 import { Button, TextField, Typography } from '@material-ui/core'
-import Slider from '@material-ui/core/Slider';
+import DateTimePicker from 'react-datetime-picker';
+//import Slider from '@material-ui/core/Slider';
 // const useStyles = makeStyles((theme) => ({
 //     root: {
 //       display: 'flex',
@@ -15,24 +16,39 @@ import Slider from '@material-ui/core/Slider';
 //   }));
 export default function Tweet() {
     // const classes = useStyles();
-    const [date,setDate]=React.useState(0);
+    const today=new Date();
+    const [date,setDate]=React.useState(new Date());
     const [content,setContent]=React.useState('');
     const [click,setClick]=React.useState(false);
+    const [difference,setDifference]=React.useState({day:0,year:0,month:0,hour:0,min:0})
     React.useEffect(() => {                
     }, [])
     function handleClick()
     {
         setClick(true);
         setTimeout(()=>{        
-            setContent('')},86400*date);
-    }
-    function valuetext(value) {
-        return `${value}°C`;
-      }            
-      const handleChange = (event, newValue) => {
-        setDate(newValue);
-        
-      };
+            setContent('')},1000*(difference.day*86400+difference.month*86400*30+difference.year*364*86400+difference.hour*3600+difference.min*60));
+    }    
+
+    // var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    // var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    // var dateTime = date+' '+time;             
+    const handleDateChange = (date) => {
+        if (date!==null)
+        {
+            setDate(date);
+            const days=date.getDate()-today.getDate();
+            const years=date.getFullYear()-today.getFullYear();
+            const months=date.getMonth()-today.getMonth();
+            const hours=date.getHours()-today.getHours();
+            const mins=date.getMinutes()-today.getMinutes();
+            setDifference({day:days,year:years,month:months,hour:hours,min:mins})
+            console.log(difference);
+        }
+        else{
+            setDate(date)
+        }
+    };
     return (
         <div>
             <TextField
@@ -49,7 +65,7 @@ export default function Tweet() {
                 }}
                 variant="filled"
             />
-             <Slider
+             {/* <Slider
                 defaultValue={1}
                 getAriaValueText={valuetext}
                 aria-labelledby="discrete-slider"
@@ -60,7 +76,13 @@ export default function Tweet() {
                 marks
                 min={1}
                 max={10}
+            /> */}
+            <DateTimePicker
+                onChange={handleDateChange}
+                value={date}
             />
+            <br />
+            <br />
             <Button style={{borderRadius:'5vh',backgroundColor:'lightblue'}} onClick={handleClick}>Tweet</Button>           
             {content!=='' && click===true && (
                 <Typography>
