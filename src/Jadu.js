a7d3f817c0afe60fa5ab6dca8546b88e28c4ccbc
ishan-jaiswal <devhjaiswal@gmail.com>
2020-09-07 20:24:39 +0530

    completed

--- src/Jadu.js
+++ src/Jadu.js
@@ -18,42 +18,42 @@ export default function Jadu(props) {
     const classes = useStyles();
     const [check,setCheck]=React.useState(false);
     React.useEffect(() => {
-        setTimeout(()=>{
-            if(new Date()<=props.exp)
+        setInterval(() => {
+            if(new Date()>=props.exp)
             {
-                setCheck(true)
+                setCheck(false)            
             }
             else
             {
-                setCheck(false)
-            }
-        },1000);
-    }, [props.exp])
+                setCheck(true)
+            }    
+          }, 1000);                    
+    }, [])
     return (
         <div>
             {check===true && (
-            <Card className={classes.root} raised={true}>
-            <CardContent>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    Tweet
-                </Typography>
-                <Typography variant="h5" component="h2">
-                    {props.item}
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                    Expiry Date
-                </Typography>
-                <Typography variant="body2" component="p">
-                    {props.exp.getDate()+'/'+(props.exp.getMonth()+1)+'/'+props.exp.getFullYear()}
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                    Expiry Time
-                </Typography>
-                <Typography variant="body2" component="p">
-                    {props.exp.getHours()+':'+props.exp.getMinutes()}
-                </Typography>
-            </CardContent>                    
-        </Card>
+                <Card className={classes.root} raised={true}>
+                    <CardContent>
+                        <Typography className={classes.title} color="textSecondary" gutterBottom>
+                            Tweet
+                        </Typography>
+                        <Typography variant="h5" component="h2">
+                            {props.item}
+                        </Typography>
+                        <Typography className={classes.pos} color="textSecondary">
+                            Expiry Date
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                            {props.exp.getDate()+'/'+(props.exp.getMonth()+1)+'/'+props.exp.getFullYear()}
+                        </Typography>
+                        <Typography className={classes.pos} color="textSecondary">
+                            Expiry Time
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                            {props.exp.getHours()+':'+props.exp.getMinutes()}
+                        </Typography>
+                    </CardContent>                    
+                </Card>
             )}
         </div>
     )
